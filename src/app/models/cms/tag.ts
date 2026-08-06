f229b363337f5f425c115e299ce2525a77cd3690
estellise-yukihime <shiranoyukihime@gmail.com>
2026-08-06 21:22:56 +0800

    feat: add `Tag` TypeScript model for CMS integration

---
+++ src/app/models/cms/tag.ts
@@ -0,0 +1,10 @@
+export type Tag = {
+  id: number
+  documentId: string | null
+  name: string | null
+  slug: string | null
+  description: string | null
+  createdAt: string | null
+  updatedAt: string | null
+  publishedAt: string | null
+}
